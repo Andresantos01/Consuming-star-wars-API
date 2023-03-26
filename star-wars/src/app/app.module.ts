6c6a9bb857e5befc9945407aed5475c61a656e55
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-26 01:48:48 -0300

    Update setup initial

--- star-wars/src/app/app.module.ts
+++ star-wars/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './components/card/card.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
