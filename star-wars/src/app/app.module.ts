6a235097a9d449bc8dc83beb639ff788d2151f78
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-26 19:18:08 -0300

    Update app module

--- star-wars/src/app/app.module.ts
+++ star-wars/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
-import { HomeComponent } from './components/home/home.component';
-
+import { HomeComponent } from './views/home/home.component';
+import {HttpClientModule } from '@angular/common/http';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import { PeopleComponent } from './views/people/people.component';
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    HomeComponent
+    HomeComponent,
+    PeopleComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatListModule,
+    MatButtonModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
