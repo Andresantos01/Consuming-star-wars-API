9e01182e76cf35e4ac8a0e81948c1b8798c5e55f
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-26 20:49:44 -0300

    Update home component
    add service api

--- star-wars/src/app/views/home/home.component.ts
+++ star-wars/src/app/views/home/home.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IStarWars } from 'src/app/models/IStarWars';
+import { StarWarsService } from 'src/app/services/starwars.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
+  providers: [StarWarsService]
 })
 
 
 
 export class HomeComponent implements OnInit {
+
+
+  dataStarWars!: IStarWars;
+  constructor(
+    public starwarsService: StarWarsService
+    ){
+    this.starwarsService.getRoutes().subscribe((route: IStarWars)=>{
+      
+      this.dataStarWars = route;
+      console.log(this.dataStarWars)
+    })
+  }
+
+
 
   ngOnInit() :void {}
   
