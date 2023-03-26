7b88fa44f478d745a1d92bef322bd21918f4dede
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-26 20:49:19 -0300

    Update card component
    Migrate service to component home

--- star-wars/src/app/components/card/card.component.ts
+++ star-wars/src/app/components/card/card.component.ts
@@ -1,29 +1,17 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { IStarWars } from 'src/app/models/IStarWars';
-import { StarWarsService } from 'src/app/services/starwars.service';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css'],
-  providers: [StarWarsService]
+  styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit{
     @Input()
     category!:String;
     @Input()
     linkTo!: String;
-
-    dataStarWars!: IStarWars;
-    constructor(
-      public starwarsService: StarWarsService
-      ){
-      this.starwarsService.getRoutes().subscribe((route: IStarWars)=>{
-        
-        this.dataStarWars = route;
-        console.log(this.dataStarWars)
-      })
-    }
-
+    @Input()
+    starWarsApi!: String;
+   
     ngOnInit() : void {}
 }
