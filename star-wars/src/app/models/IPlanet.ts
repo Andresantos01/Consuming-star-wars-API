607b265654656e904bdf0d5ac60128bfa872a039
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-27 02:14:00 -0300

    Create interface IPlanet

---
+++ star-wars/src/app/models/IPlanet.ts
@@ -0,0 +1,16 @@
+export interface IPlanet {
+    name: String;
+    rotation_period: Number;
+    orbital_period: Number;
+    diameter: Number;
+    climate: String;
+    gravity: String;
+    terrain: String;
+    surface_water: Number;
+    population: Number;
+    residents: [];
+    films: [];
+    created: string;
+    edited: String;
+    url: String;
+}
