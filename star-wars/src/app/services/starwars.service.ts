deab79d169650dd1c2fd8264f417ecb8cd2b8ecb
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-27 03:07:34 -0300

    Create get movie by id

--- star-wars/src/app/services/starwars.service.ts
+++ star-wars/src/app/services/starwars.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
+import { IMovie } from '../models/IMovie';
 import { IPeople } from '../models/IPeople';
 import { IPeopleService } from '../models/IPeopleService';
 import { IPlanet } from '../models/IPlanet';
@@ -22,5 +23,9 @@ export class StarWarsService {
 
     getPlanet(planetId: Number) : Observable<IPlanet>{
         return this.http.get<IPlanet>(this.elementApiUrl + 'planets/' + planetId)
+    }
+
+    getFilm(movieId: Number) : Observable<IMovie>{
+        return this.http.get<IMovie>(this.elementApiUrl + 'films/' + movieId)
     }
 }
