4a57446ad998188c357569fea79e7d6f58f82337
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-26 22:17:48 -0300

    Create Interface IPeople
    this interface IPeople represents the object returned by api

---
+++ star-wars/src/app/models/IPeople.ts
@@ -0,0 +1,18 @@
+export interface IPeople{
+    name: String;
+    height: Number;
+    mass: Number;
+    hair_color: String;
+    skin_color: string;
+    eye_color: String;
+    birth_year: String;
+    gender: String;
+    homeworld: String;
+    films: [];
+    species: [];
+    vehicles: [];
+    starships: [];
+    created: String;
+    edited: String;
+    url: String;
+}
