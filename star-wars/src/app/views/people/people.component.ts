294acf28db1f582db6b8b681261a8b0653619016
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-26 22:16:23 -0300

    Create dinamic table

--- star-wars/src/app/views/people/people.component.ts
+++ star-wars/src/app/views/people/people.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild } from '@angular/core';
 import { IPeopleService } from 'src/app/models/IPeopleService';
 import { StarWarsService } from 'src/app/services/starwars.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import { IPeople } from 'src/app/models/IPeople';
+
 
 @Component({
   selector: 'app-people',
@@ -8,17 +13,26 @@ import { StarWarsService } from 'src/app/services/starwars.service';
   styleUrls: ['./people.component.css'],
   providers: [StarWarsService]
 })
+
+
+
 export class PeopleComponent implements OnInit {
-  
+
+  @ViewChild(MatTable)
+  table !: MatTable <any>
+  headerColumnsPeople: string[] = ['name', 'height', 'mass', 'hair_color', 'skin_color', 'eye_color', 'birth_year','gender']
+
   dataPeople!: IPeopleService;
+  dataSource!: IPeople[]
 
   constructor(
     public starwarsService: StarWarsService
     ){
-    this.starwarsService.listAllPeople().subscribe((route: IPeopleService)=>{
+    this.starwarsService.listAllPeople().subscribe((data: IPeopleService)=>{
       
-      this.dataPeople = route;
-      console.log(this.dataPeople)
+      this.dataPeople = data;
+      this.dataSource = data.results;
+      console.log(this.dataSource);
     })
   }
 
