0bde6b0e944c50841e54d97a25bbfa1f7e62526a
Andresantos01 <68791372+Andresantos01@users.noreply.github.com>
2023-03-29 08:06:20 -0300

    Implement method get planets

--- star-wars/src/app/views/planets/planets.component.ts
+++ star-wars/src/app/views/planets/planets.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IServiceApi } from 'src/app/models/IPeopleService';
+import { IPlanet } from 'src/app/models/IPlanet';
+import { StarWarsService } from 'src/app/services/starwars.service';
 
 @Component({
   selector: 'app-planets',
   templateUrl: './planets.component.html',
-  styleUrls: ['./planets.component.css']
+  styleUrls: ['./planets.component.css'],
+  providers: [StarWarsService]
 })
-export class PlanetsComponent {
+export class PlanetsComponent implements OnInit  {
+  dataSourcePlanet!: IPlanet[];
 
+  constructor(
+    public starwarsService: StarWarsService
+  ){
+    this.starwarsService.getPlanets().subscribe((data : IServiceApi) => {
+      this.dataSourcePlanet = data.results;
+      console.log(data);
+    })
+  }
+  ngOnInit() {}
 }
